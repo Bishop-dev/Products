048976b9785dbcf533eb3a77e80635130c8af297
Oksana-Shevchenko <oksana-shevchenko@ukr.net>
2021-11-05 21:13:06 +0200

    Modify item, nutrition entity, add nutrition repository
    Add string converter for ingredients field.
    Add text type for description, ingredients columns.

--- src/main/java/com/inflation/products/entity/ItemEntity.java
+++ src/main/java/com/inflation/products/entity/ItemEntity.java
@@ -1,5 +1,6 @@
 package com.inflation.products.entity;
 
+import com.inflation.products.converter.StringListConverter;
 import com.inflation.products.entity.enums.MeasureUnits;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,6 +10,7 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Convert;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -17,6 +19,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import java.util.List;
 
 @Data
 @Builder
@@ -25,76 +28,77 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "item")
 public class ItemEntity {
-  @Id
-  @GeneratedValue(generator = "uuid")
-  @GenericGenerator(name = "uuid", strategy = "uuid2")
-  private String id;
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
 
-  @Column(name = "custom_id")
-  private String customId;
+    @Column(name = "custom_id")
+    private String customId;
 
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "category_id", referencedColumnName = "id")
-  private CategoryEntity category;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category_id", referencedColumnName = "id")
+    private CategoryEntity category;
 
-  @Column(name = "sku")
-  private String sku;
+    @Column(name = "sku")
+    private String sku;
 
-  @Column(name = "title")
-  private String title;
+    @Column(name = "title")
+    private String title;
 
-  @Column(name = "weight")
-  private double weight;
+    @Column(name = "weight")
+    private double weight;
 
-  @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "trademark_id", referencedColumnName = "id")
-  private TrademarkEntity trademark;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "trademark_id", referencedColumnName = "id")
+    private TrademarkEntity trademark;
 
-  @Column(name = "shopId")
-  private String shopId;
+    @Column(name = "shopId")
+    private String shopId;
 
-  @Column(name = "bundle")
-  private int bundle;
+    @Column(name = "bundle")
+    private int bundle;
 
-  @Column(name = "unit")
-  private String unit;
-  // облагаемый акцизом?
-  @Column(name = "excisable")
-  private Boolean excisable;
+    @Column(name = "unit")
+    private String unit;
+    // облагаемый акцизом?
+    @Column(name = "excisable")
+    private Boolean excisable;
 
-  @Column(name = "fat")
-  private double fat;
+    @Column(name = "fat")
+    private double fat;
 
-  // сделать конвертер чтобы это в базе хранилось как строка, а в java-entity было в виде
-  // List<String>
-  // сделать колонку типа TEXT
-  @Column(name = "ingredients")
-  private String ingredients;
+    @Convert(converter = StringListConverter.class)
+    @Column(name = "ingredients", columnDefinition = "TEXT")
+    private List<String> ingredients;
 
-  @Column(name = "temperature_min")
-  private int temperatureMin;
+    @Column(name = "temperature_min")
+    private int temperatureMin;
 
-  @Column(name = "temperature_max")
-  private int temperatureMax;
+    @Column(name = "temperature_max")
+    private int temperatureMax;
 
-  @Column(name = "is_alcohol")
-  private Boolean isAlcohol;
+    @Column(name = "is_alcohol")
+    private Boolean isAlcohol;
 
-  @Column(name = "is_nictotine")
-  private Boolean isNicotine;
+    @Column(name = "is_nictotine")
+    private Boolean isNicotine;
 
-  @Column(name = "units")
-  @Enumerated(EnumType.STRING)
-  private MeasureUnits units;
+    @Column(name = "units")
+    @Enumerated(EnumType.STRING)
+    private MeasureUnits units;
 
-  @Column(name = "shelf_life")
-  private double shelfLife;
+    @Column(name = "shelf_life")
+    private double shelfLife;
 
-  @Column(name = "shelf_life_units")
-  @Enumerated(EnumType.STRING)
-  private MeasureUnits shelfLifeUnits;
+    @Column(name = "shelf_life_units")
+    @Enumerated(EnumType.STRING)
+    private MeasureUnits shelfLifeUnits;
 
-  // сделать TEXT in db
-  @Column(name = "description")
-  private String description;
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "nutrition_id", referencedColumnName = "id")
+    private NutritionEntity nutritionEntity;
 }
--- src/main/java/com/inflation/products/entity/NutritionEntity.java
+++ src/main/java/com/inflation/products/entity/NutritionEntity.java
@@ -2,41 +2,51 @@ package com.inflation.products.entity;
 
 import com.inflation.products.entity.enums.MeasureUnits;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
-/** Хранение состава продукта: калорийность, жиры, углеводы */
+/**
+ * Хранение состава продукта: калорийность, жиры, углеводы
+ */
 @Data
 @Entity
 public class NutritionEntity {
-  @Column(name = "protein_amount")
-  private double proteinAmount;
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "protein_units")
-  private MeasureUnits proteinUnits;
+    @Column(name = "protein_amount")
+    private double proteinAmount;
 
-  @Column(name = "fat_amount")
-  private double fatAmount;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "protein_units")
+    private MeasureUnits proteinUnits;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "fat_units")
-  private MeasureUnits fatUnits;
+    @Column(name = "fat_amount")
+    private double fatAmount;
 
-  @Column(name = "carbohydrates_amount")
-  private double carbohydratesAmount;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "fat_units")
+    private MeasureUnits fatUnits;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "carbohydrates_units")
-  private MeasureUnits carbohydratesUnits;
+    @Column(name = "carbohydrates_amount")
+    private double carbohydratesAmount;
 
-  @Column(name = "energy_amount")
-  private double energyAmount;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "carbohydrates_units")
+    private MeasureUnits carbohydratesUnits;
 
-  @Enumerated(EnumType.STRING)
-  @Column(name = "energy_units")
-  private MeasureUnits energyUnits;
+    @Column(name = "energy_amount")
+    private double energyAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "energy_units")
+    private MeasureUnits energyUnits;
 }
